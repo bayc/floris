3e2715c50c02be8999d8f306b56c5aa84f4284a5
Chris Bay <christopher.j.bay@gmail.com>
2019-12-30 14:37:57 -0700

    adding functions to edit shapes

--- floris/simulation/julia/shapes.jl
+++ floris/simulation/julia/shapes.jl
@@ -1,22 +1,22 @@
 abstract type AbstractShape end
 
-struct Cube <: AbstractShape    
-    x
-    y
-    z
-    name
+mutable struct Cube <: AbstractShape    
+    x::Float64
+    y::Float64
+    z::Float64
+    name::String
 end
 
-struct Sphere <: AbstractShape
-    r
-    name
+mutable struct Sphere <: AbstractShape
+    r::Float64
+    name::String
 end
 
-struct Pyramid <: AbstractShape
-    l
-    w
-    h
-    name
+mutable struct Pyramid <: AbstractShape
+    l::Float64
+    w::Float64
+    h::Float64
+    name::String
 end
 
 function volume(shape::Cube)
@@ -29,4 +29,46 @@ end
 
 function volume(shape::Pyramid)
     return shape.l * shape.w * shape.h / 3
+end
+
+function edit_shape(shape::Cube; x=nothing, y=nothing, z=nothing, name=nothing)
+    if x !== nothing
+        shape.x = x
+    end
+    if y !== nothing
+        shape.y = y
+    end
+    if z !== nothing
+        shape.z = z
+    end
+    if name !== nothing
+        shape.name = name
+    end
+    return shape
+end
+
+function edit_shape(shape::Sphere; r=nothing, name=nothing)
+    if r !== nothing
+        shape.r = r
+    end
+    if name !== nothing
+        shape.name = name
+    end
+    return shape
+end
+
+function edit_shape(shape::Pyramid; l=nothing, w=nothing, h=nothing, name=nothing)
+    if l !== nothing
+        shape.l = l
+    end
+    if w !== nothing
+        shape.w = w
+    end
+    if h !== nothing
+        shape.h = h
+    end
+    if name !== nothing
+        shape.name = name
+    end
+    return shape
 end
